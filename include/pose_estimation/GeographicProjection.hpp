3d4553cf3445c55e982468b40537449317cf369d
Tom Creutz <tom.creutz@dfki.de>
2023-10-18 12:06:38 +0200

    changes everything to work nicely with ros2 humble

--- include/pose_estimation/GeographicProjection.hpp
+++ include/pose_estimation/GeographicProjection.hpp
@@ -1,7 +1,6 @@
 #ifndef _POSE_ESTIMATION_PROJECTED_COORDINATE_SYSTEM_HPP
 #define _POSE_ESTIMATION_PROJECTED_COORDINATE_SYSTEM_HPP
 
-#include <boost/noncopyable.hpp>
 #include <Eigen/Geometry>
 
 class OGRCoordinateTransformation;
@@ -9,24 +8,28 @@ class OGRCoordinateTransformation;
 namespace pose_estimation
 {
 
-    /**
+/**
      * Helper class to project from GPS coordinates to a local
      * plane in NWU coordinates and vice versa.
      */
-    class GeographicProjection : private boost::noncopyable
-    {
-    public:
-        GeographicProjection(double latitude, double longitude, double x = 0., double y = 0.);
-        virtual ~GeographicProjection();
+class GeographicProjection
+{
+public:
+  GeographicProjection(double latitude, double longitude, double x = 0., double y = 0.);
+  virtual ~GeographicProjection();
 
-        bool worldToNav(double latitude, double longitude, double &x, double &y);
-        bool navToWorld(double x, double y, double &latitude, double &longitude);
+  // makes the class uncopyable
+  GeographicProjection(const GeographicProjection &) = delete;
+  GeographicProjection & operator=(const GeographicProjection &) = delete;
 
-    protected:
-        OGRCoordinateTransformation *world2nav;
-        OGRCoordinateTransformation *nav2world;
-        Eigen::Vector2d offset;
-    };
+  bool worldToNav(double latitude, double longitude, double & x, double & y);
+  bool navToWorld(double x, double y, double & latitude, double & longitude);
+
+protected:
+  OGRCoordinateTransformation * world2nav;
+  OGRCoordinateTransformation * nav2world;
+  Eigen::Vector2d offset;
+};
 
 }
 
